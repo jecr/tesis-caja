ed119ec3b9ea67467a6269cbbc998a0d4e44bc4b
Jorge Cortés <jorge.ecro@gmail.com>
2016-02-29 13:04:46 -0600

    Update on API search

--- filtering/tweepy_test.py
+++ filtering/tweepy_test.py
@@ -1,4 +1,7 @@
+# -*- coding: UTF-8 -*-
+# Búsqueda de tweets por término
 import tweepy
+import os
 
 consumer_key = 'ZR2SU5TrGKQ2zCbVbyDUw'
 consumer_secret = 'Rcg5Esw9z6z8JdIEfJIp4NBRzgxA3i6ISeCL1mDM'
@@ -11,30 +14,9 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-# Consigue al usuario
-# user = api.get_user('jorgetuitea')
-
-# public_tweets = api.user_timeline( user )
-# for tweet in public_tweets:
-#     print tweet.text
-
-
-
-# for follower in user.followers():
-# 	print 'Usuario: '+follower.screen_name+' Bio: '+follower.description
-
-
-
-
-# ===== Recupera las descripciones de los seguidores de los seguidores del usuario
-# for follower in user.followers():
-# 	for algo in follower.followers():
-# 		print follower.screen_name+' : '+algo.description
-
-
-
-
-results = api.search(q="Mice")
-
-for result in results:
-    print result.text
+try:
+    for page in tweepy.Cursor(api.search, q="leonardo", rpp=15).pages(2):
+        for tweet in page:
+            print tweet.text.encode('utf8')
+except tweepy.TweepError as e:
+    print e
